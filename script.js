ee64be6ff4998ea3fa9ec5a4d17ce959d6ff398a
Grzegorz Sadowski <34543404+GrzegorzSadowski@users.noreply.github.com>
2018-03-02 10:08:13 +0000

    Update script.js

--- script.js
+++ script.js
@@ -19,7 +19,7 @@ function geoFindMe() {
       var lat = position.coords.latitude.toFixed(3);
       var lon = position.coords.longitude.toFixed(3);
       output.innerHTML=""
-      img.src = "https://maps.googleapis.com/maps/api/staticmap?center="+lat+","+lon+"&"+"zoom=13"+"&"+"size=250x250&key=key=AIzaSyDd2_sIPVL0w0N_qOuPJHYTqnJD5M73EzY";
+      img.src = "https://maps.googleapis.com/maps/api/staticmap?center="+lat+","+lon+"&"+"zoom=13"+"&"+"size=250x250+"&"+key=AIzaSyBsU_T-MjanJ0gwUK04Lo1_2zyuzzE2aHQ";
     
       output.append(img);
       getWeather(lat,lon);
